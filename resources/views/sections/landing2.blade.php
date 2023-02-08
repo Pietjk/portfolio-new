d22e4a795abf8eb1c8ba89d92dc5117902788fed
Piet <pkorfmaker@gmail.com>
2023-02-08 20:37:19 +0100

    retro layout

--- resources/views/sections/landing2.blade.php
+++ resources/views/sections/landing2.blade.php
@@ -1,36 +1,10 @@
-<section class="min-h-screen bg-gradient-to-b from-purple-600 via-orange-400 to-yellow-400" id="landing">
-    <div class="container mx-auto text-black min-h-full">
-        <header class="text-center p-4 relative z-10 overflow-visible" id="landing-heading">
-            {{-- lg:text-[6.7rem] md:text-[4.9rem] sm:text-[4.1rem] text-[2.5rem] --}}
-
-            <h1 class="lg:text-[9.1rem] md:text-[6rem] sm:text-[5rem] text-[3.9rem] overflow-visible">Piet Korfmaker</h1>
-            <h2 class="md:text-5xl text-4xl overflow-visible">Front end developer</h2>
-        </header>
-
-        <div class="mx-auto pt-4 md:w-[444px] w-[333px]  relative z-10" id="main-text-box">
-            <h3 class="text-2xl">Welkom op mijn Portfolio!</h3>
-            <p>
-                Ik ben een web applicatie ontwikkelaar, werkzaam bij
-                <u><a href="https://postis.it">Postis IT Group</a></u>.
-                Ik houd mij bezig met het maken van mooie web applicaties. Zowel Front- als Backend.
-            </p>
-            <p class="pt-5">
-                Mijn huidige specialiteiten zijn:
-                <code>php/laravel</code>,
-                <code>javascript/vue</code>,
-                <code>css/tailwind/bootstrap</code>.
-                En ik ben altijd door aan het leren.
-            </p>
-            <p class="pt-5">
-                Wil je meer weten? Klik dan op de zon. Of kijk op mijn
-                <u><a href="https://github.com/Pietjk">Github</a></u>
-                of mijn
-                <u><a href="https://www.linkedin.com/in/piet-korfmaker-404936232/">Linkedin</a></u>.
-            </p>
-        </div>
+<section class="min-h-screen bg-gradient-to-b from-slate-900 via-violet-900 to-indigo-600" id="landing">
+    <div href="#" class="sun" id="sun"></div>
+    <div class="grid-container grid grid-cols-12">
+        @for ($i = 0; $i < 116; $i++)
+            <div class="grid-line"></div>
+        @endfor
     </div>
-
-    <a href="#" class="sun" id="sun"></a>
 </section>
 <script>
      document.getElementById('sun').addEventListener('click', sunExplodes);
