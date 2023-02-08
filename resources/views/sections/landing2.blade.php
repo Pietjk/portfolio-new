a8e277de4eb3b8ed177c9bb81a0809d80050a077
Piet <pkorfmaker@gmail.com>
2023-02-08 21:00:09 +0100

    Add glow to lines

--- resources/views/sections/landing2.blade.php
+++ resources/views/sections/landing2.blade.php
@@ -1,5 +1,5 @@
 <section class="min-h-screen bg-gradient-to-b from-slate-900 via-violet-900 to-indigo-600" id="landing">
-    <div href="#" class="sun" id="sun"></div>
+    <a href="#" class="sun" id="sun"></a>
     <div class="grid-container grid grid-cols-12">
         @for ($i = 0; $i < 116; $i++)
             <div class="grid-line"></div>
