24f47ab2203ab11bca981abe4e8c1c2902084f73
Piet <pkorfmaker@gmail.com>
2023-02-10 00:36:17 +0100

    Remove old landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('sections.landing2')
+    @include('sections.landing')
     @include('sections.experiences')
 @endsection
--- resources/views/sections/experiences.blade.php
+++ resources/views/sections/experiences.blade.php
@@ -3,5 +3,26 @@
         <header class="text-center p-4">
         <h1 class="lg:text-[9.1rem] md:text-[6rem] sm:text-[5rem] text-[3.9rem]">Ervaringen</h1>
         </header>
+        <div class="mx-auto pt-4 md:w-[444px] w-[333px]  relative z-10" id="main-text-box">
+            <h3 class="text-2xl">Welkom op mijn Portfolio!</h3>
+            <p>
+                Ik ben een web applicatie ontwikkelaar, werkzaam bij
+                <u><a href="https://postis.it">Postis IT Group</a></u>.
+                Ik houd mij bezig met het maken van mooie web applicaties. Zowel Front- als Backend.
+            </p>
+            <p class="pt-5">
+                Mijn huidige specialiteiten zijn:
+                <code>php/laravel</code>,
+                <code>javascript/vue</code>,
+                <code>css/tailwind/bootstrap</code>.
+                En ik ben altijd door aan het leren.
+            </p>
+            <p class="pt-5">
+                Wil je meer weten? Klik dan op de zon. Of kijk op mijn
+                <u><a href="https://github.com/Pietjk">Github</a></u>
+                of mijn
+                <u><a href="https://www.linkedin.com/in/piet-korfmaker-404936232/">Linkedin</a></u>.
+            </p>
+        </div>
     </div>
 </section>
