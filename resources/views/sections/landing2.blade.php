63bbce3186ad944af3f7cbd2ff57f0710b6f7ac4
Piet <pkorfmaker@gmail.com>
2023-02-10 00:34:45 +0100

    Finish-ish landing page

--- resources/views/sections/landing2.blade.php
+++ resources/views/sections/landing2.blade.php
@@ -1,5 +1,8 @@
 <section class="min-h-screen bg-gradient-to-b from-slate-900 via-violet-900 to-indigo-600" id="landing">
-    <a href="#" class="sun" id="sun"></a>
+    <a href="#" class="sun overflow-visible" id="sun">
+        <img class="z-10 absolute -bottom-5" src="{{ asset('/images/piet_logo_grain.png') }}" alt="">
+    </a>
+
     <div class="grid-container grid grid-cols-12">
         @for ($i = 0; $i < 116; $i++)
             <div class="grid-line"></div>
