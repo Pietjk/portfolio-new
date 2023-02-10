82f5254b682c2583791ffd81b3d5debd9129c0b2
Piet <pkorfmaker@gmail.com>
2023-02-11 00:32:32 +0100

    Create project page

---
+++ resources/views/components/_project.blade.php
@@ -0,0 +1,5 @@
+<div class="text pb-5 md:order-{{$order[0]}} order-{{$order[1]}}">
+    <h2 class="text-xl text-[#E00496] text-shadow-pink">{{$project}}</h2>
+    <p>{!! $text !!}</p>
+</div>
+<img class="image border-4 rounded-xl border-[#09aac0] box-shadow-blue md:order-{{$order[2]}} order-{{$order[3]}}" src="{{$image}}" alt="{{ $alt }}">
--- resources/views/sections/experiences.blade.php
+++ resources/views/sections/experiences.blade.php
@@ -1,28 +1,61 @@
-<section class="min-h-screen bg-slate-700 relative z-10" id="experiences">
-    <div class="container mx-auto text-white">
-        <header class="text-center p-4">
-        <h1 class="lg:text-[9.1rem] md:text-[6rem] sm:text-[5rem] text-[3.9rem]">Ervaringen</h1>
-        </header>
-        <div class="mx-auto pt-4 md:w-[444px] w-[333px]  relative z-10" id="main-text-box">
-            <h3 class="text-2xl">Welkom op mijn Portfolio!</h3>
-            <p>
-                Ik ben een web applicatie ontwikkelaar, werkzaam bij
-                <u><a href="https://postis.it">Postis IT Group</a></u>.
-                Ik houd mij bezig met het maken van mooie web applicaties. Zowel Front- als Backend.
-            </p>
-            <p class="pt-5">
-                Mijn huidige specialiteiten zijn:
-                <code>php/laravel</code>,
-                <code>javascript/vue</code>,
-                <code>css/tailwind/bootstrap</code>.
-                En ik ben altijd door aan het leren.
-            </p>
-            <p class="pt-5">
-                Wil je meer weten? Klik dan op de zon. Of kijk op mijn
-                <u><a href="https://github.com/Pietjk">Github</a></u>
-                of mijn
-                <u><a href="https://www.linkedin.com/in/piet-korfmaker-404936232/">Linkedin</a></u>.
-            </p>
+<section class="min-h-[100vh] relative z-10" id="experiences">
+    <div class="container mx-auto text-white max-w-[1024px]">
+        @include('components._header', [$text = 'Projecten'])
+        <div class="grid md:grid-cols-2 grid-cols-1 gap-y-5 gap-x-10 px-10 py-5 projects">
+        @include('components._project', [
+                $project = 'Ruben Korfmaker',
+                $text = 'Deze website heb ik gemaakt voor mijn vader. Hij stond toen op het punt om zijn nieuwe boek uit te brengen en had daar een website bij nodig. De website heb ik geschreven in <code>PhP/Laravel</code> met een vleugje <code>Javascript</code>. En om het er een beetje mooi uit te laten zien heb ik het css framework <code>Bulma</code> gebruikt. Het heeft een hele custom backend voor het bijhouden van de data die wordt weergegeven op de voorkant.',
+                $image = asset('images/landings/rubenkorfmaker.png'),
+                $order = [1,2,2,1],
+                $alt = 'De Ruben Korfmaker landing pagina'
+        ])
+            {{-- <div class="text md:order-1 order-2">
+                <h2 class="text-xl text-[#E00496] text-shadow-pink">Ruben Korfmaker</h2>
+                <p>Deze website heb ik gemaakt voor mijn vader. Hij stond toen op het punt om zijn nieuwe boek uit te brengen en had daar een website bij nodig. De website heb ik geschreven in <code>PhP/Laravel</code> met een vleugje <code>Javascript</code>. En om het er een beetje mooi uit te laten zien heb ik het css framework <code>Bulma</code> gebruikt. Het heeft een hele custom backend voor het bijhouden van de data die wordt weergegeven op de voorkant.</p>
+            </div>
+            <img class="image border-4 rounded-xl border-[#09aac0] box-shadow-blue md:order-2 order-1" src="{{asset('images/landings/rubenkorfmaker.png')}}" alt="De Ruben Korfmaker landing pagina"> --}}
+
+            @include('components._project', [
+                $project = 'De Breimeisjes',
+                $text = 'Mijn moeder houdt al jaren een blog bij. Daarnaast had ze ook een website, maar die was absoluut aan een update toe. Ik heb deze website geschreven in <code>PhP/Laravel</code>. De styling is gedaan met <code>Bootstrap 5</code>. Deze site heeft ook een backend voor het aanpassen van de content aan de voorkant. Ook heeft deze website een connectie met de Ravelry API voor het weergeven van de producten in de Ravelry winkel.',
+                $image = asset('images/landings/debreimeisjes.png'),
+                $order = [4,4,3,3],
+                $alt = 'De Breimeisjes landing pagina'
+            ])
+
+            {{-- <img class="image border-4 rounded-xl border-[#09aac0] box-shadow-blue md:order-3 order-3" src="{{asset('images/landings/debreimeisjes.png')}}" alt="De Breimeisjes landing pagina">
+            <div class="text md:order-4 order-4">
+                <h2 class="text-xl text-[#E00496] text-shadow-pink">De Breimeisjes</h2>
+                <p>Mijn moeder houdt al jaren een blog bij. Daarnaast had ze ook een website, maar die was absoluut aan een update toe. Ik heb deze website geschreven in <code>PhP/Laravel</code>. De styling is gedaan met <code>Bootstrap 5</code>. Deze site heeft ook een backend voor het aanpassen van de content aan de voorkant. Ook heeft deze website een connectie met de Ravelry API voor het weergeven van de producten in de Ravelry winkel.</p>
+            </div> --}}
+
+            @include('components._project', [
+                $project = 'Tige Site',
+                $text = 'Deze website ben ik gestart met een paar vrienden. Het idee erachter was om een website te hebben om een aantal oefen projecten op te doen. De website draait op <code>PhP/Laravel</code> en wordt gestyled met <code>Bootstrap 5</code>. Met oefen projecten met bijvoorbeeld <code>Vue/Javascript</code>.',
+                $image = asset('images/landings/tige.png'),
+                $order = [5,6,6,5],
+                $alt = 'De Tige site landing pagina'
+            ])
+
+            {{-- <div class="text md:order-5 order-6">
+                <h2 class="text-xl text-[#E00496] text-shadow-pink">Tige Site</h2>
+                <p>Deze website ben ik gestart met een paar vrienden. Het idee erachter was om een website te hebben om een aantal oefen projecten op te doen. De website draait op <code>PhP/Laravel</code> en wordt gestyled met <code>Bootstrap 5</code>. Met oefen projecten met bijvoorbeeld <code>Vue/Javascript</code>.</p>
+            </div>
+            <img class="image border-4 rounded-xl border-[#09aac0] box-shadow-blue md:order-6 order-5" src="{{asset('images/landings/tige.png')}}" alt="De Tige site landing pagina"> --}}
+
+            @include('components._project', [
+                $project = 'Postis IT Group',
+                $text = 'De website van mijn huidige werk. Deze website heb ik helemaal van de grond af ontworpen en daarna inelkaar gestyled. Het heeft allemaal interessante coole styling elementen. Op dit moment is een groot deel nog niet zichtbaar omdat de texten daarvoor nog niet geschreven zijn. Geschreven in <code>PhP/Laravel</code> en <code>Boostrap 5</code>.',
+                $image = asset('images/landings/postis.png'),
+                $order = [8,8,7,7],
+                $alt = 'De Postis IT landing pagina'
+            ])
+
+            {{-- <img class="image border-4 rounded-xl border-[#09aac0] box-shadow-blue md:order-7 order-7" src="{{asset('images/landings/postis.png')}}" alt="De Postis IT landing pagina">
+            <div class="text md:order-8 order-8">
+                <h2 class="text-xl text-[#E00496] text-shadow-pink">Postis IT Group</h2>
+                <p>De website van mijn huidige werk. Deze website heb ik helemaal van de grond af ontworpen en daarna inelkaar gestyled. Het heeft allemaal interessante coole styling elementen. Op dit moment is een groot deel nog niet zichtbaar omdat de texten daarvoor nog niet geschreven zijn. Geschreven in <code>PhP/Laravel</code> en <code>Boostrap 5</code>.</p>
+            </div> --}}
         </div>
     </div>
 </section>
