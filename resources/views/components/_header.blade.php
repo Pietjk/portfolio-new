f9be1ba6fc1bb42a8f022b308ad23f6f1b3d90af
Pietjk <piet@postisit.nl>
2023-02-22 15:01:39 +0100

    Fix header size

--- resources/views/components/_header.blade.php
+++ resources/views/components/_header.blade.php
@@ -1,3 +1,3 @@
 <header class="text-center p-4">
-    <h1 class="lg:text-[6rem] md:text-[5rem] sm:text-[5rem] text-[3.4rem] text-[#09aac0] text-shadow-blue">{{$text}}</h1>
+    <h1 class="lg:text-[6rem] md:text-[5rem] sm:text-[5rem] text-[3rem] text-[#09aac0] text-shadow-blue">{{$text}}</h1>
 </header>
