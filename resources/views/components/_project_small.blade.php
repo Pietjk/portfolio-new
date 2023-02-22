0e5aa410dd55cfafaf599a9383e72eab9b4daeca
Pietjk <piet@postisit.nl>
2023-02-22 16:23:26 +0100

    Fix glow on icons

--- resources/views/components/_project_small.blade.php
+++ resources/views/components/_project_small.blade.php
@@ -1,8 +1,8 @@
 <div class="p-3">
     <img class="image border-4 rounded-xl border-[#09aac0] box-shadow-blue" src="{{ asset($project->image_path) }}" alt="{{ $project->title }}">
-    <h2 class="text-xl text-[#E00496] text-shadow-pink pt-3">
-        <span>{{ $project->title }}</span>
-        <span >
+    <h2 class="text-xl text-[#E00496] pt-3">
+        <span class="text-shadow-pink">{{ $project->title }}</span>
+        <span>
             @if (isset($project->site))
                 <a href="{{ $project->site }}" class="text-xl hover:text-[#ff00aa]" target="_blank"  aria-label="{{ $project->site }}">
                     <i class="fa-solid fa-arrow-up-right-from-square"></i>
