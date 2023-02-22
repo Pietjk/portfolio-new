5fccaa5f0d7c0cc35a1ce5e383e505d030a21c71
Pietjk <piet@postisit.nl>
2023-02-22 16:03:40 +0100

    Fix class typo

--- resources/views/sections/about.blade.php
+++ resources/views/sections/about.blade.php
@@ -3,11 +3,11 @@
     <div class="container mx-auto text-white max-w-[1024px] px-10">
         @if (isset($aboutHeader))
             <div class="grid md:grid-cols-3 gap-x-5 items-center mb-5">
-                <div class="col-span-2 md:order-1 order-2">
+                <div class="md:col-span-2 md:order-1 order-2">
                     <h2 class="text-[#09aac0] text-shadow-blue text-2xl">{{ $aboutHeader->title }}</h2>
                     <p>{!! $aboutHeader->text !!}</p>
                 </div>
-                <div class="px-3 pb-5 mb:p-5 md:order-2 order-1">
+                <div class="px-3 pb-5 md:p-5 md:order-2 order-1">
                     <img class="image border-4 rounded-xl border-[#09aac0] box-shadow-blue" src="{{ asset($aboutHeader->image_path) }}" alt="{{ $aboutHeader->title }}">
                 </div>
             </div>
