0dbe38303b52614f1bebffac71ba1a88d0654b4f
Piet <pkorfmaker@gmail.com>
2023-07-24 23:01:53 +0200

    More  synthwave

--- resources/views/sections/landing.blade.php
+++ resources/views/sections/landing.blade.php
@@ -1,13 +1,15 @@
-<section class="min-h-screen bg-gradient-to-b from-slate-900 via-violet-900 to-indigo-600 w-full overflow-hidden" id="landing">
-    <a href="#" class="logo" id="sun-img">
-        <img class="z-10 absolute -bottom-5" src="{{ asset('/images/piet_logo_grain.png') }}" alt="Piet jouke Korfmaker">
-    </a>
-    <a href="#" class="sun overflow-visible" id="sun" aria-label="scroll down"></a>
+<section class="min-h-screen bg-gradient-to-b from-slate-900 via-violet-900 to-indigo-600 w-full overflow-hidden landing" id="landing">
+    <div class="landing-noise">
+        <a href="#" class="logo" id="sun-img">
+            <img class="z-10 absolute -bottom-5" src="{{ asset('/images/piet_logo_grain.png') }}" alt="Piet jouke Korfmaker">
+        </a>
+        <a href="#" class="sun overflow-visible" id="sun" aria-label="scroll down"><div class="landing-noise-2 h-full w-full rounded-full"></div></a>
 
-    <div class="grid-container grid grid-cols-12">
-        @for ($i = 0; $i < 117; $i++)
-            <div class="grid-line"></div>
-        @endfor
+        <div class="grid-container grid grid-cols-12">
+            @for ($i = 0; $i < 117; $i++)
+                <div class="grid-line"></div>
+            @endfor
+        </div>
     </div>
 </section>
 @push('scripts')
