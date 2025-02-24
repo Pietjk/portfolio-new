589d49c43c586d31b1c5a7ff37622b513b78436c
Pietjk <piet@postisit.nl>
2025-02-24 16:27:56 +0100

    Remove th cool bike feature
    :(

--- resources/views/sections/landing.blade.php
+++ resources/views/sections/landing.blade.php
@@ -1,8 +1,4 @@
 <section class="min-h-screen w-full overflow-hidden landing" id="landing">
-    <div class="w-1/4 absolute z-10 hidden xl:block" style="top: 60%; left: 5%; scale: 0.700" id="follower">
-        <img src="{{ asset('/images/bike.png') }}" alt="">
-    </div>
-
     <div class="absolute z-10  hidden trail-shadow" id="trail"></div>
 
     <div id="nav-mesh" class="w-full h-[380px] absolute bottom-0 right-0 z-[11]"></div>
@@ -56,68 +52,6 @@
                 sun.classList.toggle('z-20');
                 img.classList.toggle('hidden');
             }, 3000);
-        }
-
-        let oldX = 0;
-        let follower = document.getElementById('follower');
-        let navMesh = document.getElementById('nav-mesh');
-        let trail = document.getElementById('trail');
-
-        navMesh.addEventListener("mousemove", function (event) {
-            if (document.body.offsetWidth >= 1280) {
-                follow(event)
-            }
-        });
-
-        function follow (event) {
-            let posX = event.pageX
-            let posY = event.pageY
-            let edge = navMesh.offsetWidth - follower.offsetWidth;
-            let trailX = posX;
-
-
-            if (event.pageX < oldX) {
-                follower.style.transform = 'rotateY(180deg)';
-                trailX = posX + follower.offsetWidth;
-            } else if (event.pageX > oldX) {
-                follower.style.transform = 'rotateY(0deg)';
-            }
-
-            if (posX >= edge) {
-                posX = edge;
-                trailX = edge + follower.offsetWidth;
-            }
-
-            let carPosY = posY - follower.offsetHeight / 1.3;
-
-            follower.style.top = carPosY+'px';
-            follower.style.left = posX+'px';
-            follower.style.scale = posY/1500;
-
-            let clone = cloneFactory(posY - follower.offsetHeight/2, trailX);
-
-            setTimeout(function () {
-                clone.remove();
-            }, 1000);
-
-            oldX = event.pageX
-        }
-
-        function cloneFactory (posY, posX) {
-            let clone = trail.cloneNode()
-
-            clone.removeAttribute('id');
-            clone.classList.add('xl:block');
-
-            let x = posX;
-            let y = posY;
-            clone.style.left = x +'px';
-            clone.style.top = y +'px';
-            clone.style.scale = posY/1000;
-
-            document.body.appendChild(clone)
-
-            return clone;
         }
     </script>
 @endpush
