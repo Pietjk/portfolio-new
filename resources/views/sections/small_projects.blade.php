caedfed7a31f07200321c27bcb8a130f74b7fc92
Pietjk <piet@postisit.nl>
2025-02-24 15:41:35 +0100

    Remove hardcoded video

--- resources/views/sections/small_projects.blade.php
+++ resources/views/sections/small_projects.blade.php
@@ -6,22 +6,6 @@
                 @include('components._project_small', [$project])
             @endforeach
         </div>
-
-        <div class="px-10 py-5">
-            <iframe class="w-full aspect-video image border-4 rounded-xl border-primary box-shadow-blue" src="https://www.youtube.com/embed/OAPPCnu2SRs" title="isle of greed showcase" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-            <h2 class="text-xl text-secondary pt-3">
-                <span class="text-shadow-pink">Isle of Greed</span>
-                <span>
-                    <a href="https://www.youtube.com/watch?v=OAPPCnu2SRs" class="text-xl hover:text-[#ff00aa]" target="_blank"  aria-label="youtube">
-                        <i class="fab fa-youtube"></i>
-                    </a>
-                    <a href="https://github.com/Pietjk/Isle_of_Greed" class="text-xl hover:text-[#ff00aa]" target="_blank" aria-label="github">
-                        <i class="fa-brands fa-github"></i>
-                    </a>
-                </span>
-            </h2>
-            <p>Deze <code>Unity/C#</code> game heb ik ontwikkeld in samenwerking met een vriend. Dit was het afsluitende schoolproject waardoor ik mijn eindstage mocht gaan doen.</p><p class="mt-5">De locaties van de eilanden en de sleutel fragmenten worden random gegenereerd. Met de fragmenten kan de speler een kist op het eiland openen voor een kristal. Met deze kristallen kan de speler upgrades kopen om sneller fragmenten te kunnen vinden.</p>
-        </div>
     </div>
 
     <img class="pt-10 car" src="{{asset('images/car.png')}}" alt="" id="car">
